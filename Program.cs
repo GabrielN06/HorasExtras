7f6e41497a72610bfed214842927f40531b53020
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-05-04 21:32:03 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿    Console.Write("Salário por hora: ");
+     double salarioHora = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Horas trabalhadas: ");
+     double horas = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Horas extras: ");
+     double horasExtras = double.Parse(Console.ReadLine()!);
+
+     double salario = (salarioHora * horas) + (salarioHora * 1.4 * horasExtras);
+    Console.WriteLine($"Salário final: R$ {salario:F2}");
+    
